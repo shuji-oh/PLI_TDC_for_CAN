022bdc3391c3cf51bca534203bc8922c74149e29
Shuji Ohira <ohira.shuji.ok2@is.naist.jp>
2020-12-07 14:57:46 +0900

    Update gen_LearningData.sh

--- machine_learning/real-vehicle/gen_LearningData.sh
+++ machine_learning/real-vehicle/gen_LearningData.sh
@@ -2,16 +2,16 @@
 
 echo "mean,stdev,variance,skew,kurtosis,max,min,rms,en,fine_stdev,label\n" > delay_time.csv
 # power steering
-python3 raw2delay.py delay_262.txt | sed 's/\ /,/g' | sed 's/$/,ECU0/' | tail -n 1000 >> delay_time.csv 
+python3 raw2delay.py delay_262.dat | sed 's/\ /,/g' | sed 's/$/,ECU0/' | tail -n 1000 >> delay_time.csv 
 # combination meter
-python3 raw2delay.py delay_meter.txt | sed 's/\ /,/g' | sed 's/$/,ECU1/' | tail -n 1000 >> delay_time.csv
+python3 raw2delay.py delay_meter.dat | sed 's/\ /,/g' | sed 's/$/,ECU1/' | tail -n 1000 >> delay_time.csv
 # steering
-python3 raw2delay.py delay_025.txt | sed 's/\ /,/g' | sed 's/$/,ECU2/' | tail -n 1000 >> delay_time.csv
+python3 raw2delay.py delay_025.dat | sed 's/\ /,/g' | sed 's/$/,ECU2/' | tail -n 1000 >> delay_time.csv
 # skid control
-python3 raw2delay.py delay_0B4.txt | sed 's/\ /,/g' | sed 's/$/,ECU3/' | tail -n 1000 >> delay_time.csv
+python3 raw2delay.py delay_0B4.dat | sed 's/\ /,/g' | sed 's/$/,ECU3/' | tail -n 1000 >> delay_time.csv
 # transmission control
-python3 raw2delay.py delay_127.txt | sed 's/\ /,/g' | sed 's/$/,ECU4/' | tail -n 1000 >> delay_time.csv
+python3 raw2delay.py delay_127.dat | sed 's/\ /,/g' | sed 's/$/,ECU4/' | tail -n 1000 >> delay_time.csv
 # engine control
-python3 raw2delay.py delay_1C4.txt | sed 's/\ /,/g' | sed 's/$/,ECU5/' | tail -n 1000 >> delay_time.csv
+python3 raw2delay.py delay_1C4.dat | sed 's/\ /,/g' | sed 's/$/,ECU5/' | tail -n 1000 >> delay_time.csv
 # unkown
-python3 raw2delay.py delay_1CE.txt | sed 's/\ /,/g' | sed 's/$/,ECU6/' | tail -n 1000 >> delay_time.csv
+python3 raw2delay.py delay_1CE.dat | sed 's/\ /,/g' | sed 's/$/,ECU6/' | tail -n 1000 >> delay_time.csv
